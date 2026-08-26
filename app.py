d871f3d86b03c7cf7340c819fbb7024a4b4d9f29
abdullahminhas <aminhas1996@gmail.com>
2026-08-26 22:50:57 +0500

    Validate setup and auth flow

--- app.py
+++ app.py
@@ -24,15 +24,13 @@ def load_user(user_id):
     return User.query.get(int(user_id))
 
 
-with app.app_context():
-    db.create_all()
-
-
 @app.route("/")
 def home():
     return render_template("index.html")
 
 
 if __name__ == "__main__":
+    with app.app_context():
+        db.create_all()
     app.run(debug=True)
 
